36cb65249f436ff53afd10711f3eae2b80491b55
Emman <jlr0020@alu.medac.es>
2025-09-29 17:43:59 +0200

    actualizacion

---
+++ TEMA 2/box_model.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+        .caja{
+            width: 200px ;
+            height:100px ;
+            padding: 10px 5px 10px 5px ;
+            margin: 5px;
+            border: 5px solid black ;
+            background-color: lightcoral;
+        }
+    </style>
+    
+</head>
+<body>
+    <div class="caja"></div>
+</body>
+</html>
