2f7babd0523aa2b669911cd6120d62a1e5dfae03
Emman <jlr0020@alu.medac.es>
2025-10-20 19:14:08 +0200

    update

---
+++ TEMA 4/breakpoints.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-xl-3 bg-danger">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure distinctio exercitationem recusandae repudiandae ipsam tempore neque sequi, quam ducimus sapiente cumque repellendus eaque rem, harum doloremque natus eligendi officia reiciendis.
+            </div>
+            <div class="col-12 col-md-6 col-xl-3 bg-info">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae esse eius quae officiis. Ducimus delectus odit labore enim doloremque, a vel velit iste, magnam, sapiente ex temporibus veniam exercitationem rerum.
+            </div>
+            <div class="col-12 col-md-6 col-xl-3 bg-primary">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos praesentium veniam cumque quisquam. Aliquid beatae quos provident in consectetur accusamus, obcaecati, deleniti numquam ab modi quam reiciendis nostrum excepturi rem?
+            </div>
+            <div class="col-12 col-md-6 col-xl-3 bg-success">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam exercitationem soluta ad fugiat consequuntur maxime, fuga alias vitae sequi, animi quo architecto laudantium distinctio cupiditate officia dolorem neque. Enim, eligendi?col-md-6.
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ TEMA 4/carousel.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+        <nav class="navbar navbar-expand-lg bg-warning">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto p-2 p-1">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Carousel</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Cards</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Footer</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  </nav>
+    </header>
+
+    <section>
+        <div id="carouselExampleDark" class="carousel carousel-dark slide">
+
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="Cat4.jpg" class="d-block w-100" alt="cat">
+      <div class="carousel-caption d-none d-md-block">
+        
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="Cat2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+       
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="Cat3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true" ></span>
+    
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+   
+  </button>
+</div>
+    </div>
+
+    </section>
+    <section>
+         
+
+    </section>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
