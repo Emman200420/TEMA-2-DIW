1b06472bf1b27d0f3f302fe526611ee1fd606019
Emman <jlr0020@alu.medac.es>
2025-11-10 16:42:20 +0100

    actualizacion

---
+++ TEMA 5 y 6/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primer ejercicio de sass</title>
+    <link rel="stylesheet" href="styles.css" >
+</head>
+<body>
+    <article class="articulo">
+        <h1 class="titulo">Titulo del articulo</h1>
+        <p class="autor">Emman</p>
+        <p>Lorem Ipsum is simply <a href="#">hola </a>dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <div>
+        <a href="#"> leer mas </a>
+    </div>
+    </article>
+
+</body>
+</html>
